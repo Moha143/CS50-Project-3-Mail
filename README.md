6ec82173c08981ae99dea160bd45790b97396927
Mohamed Omar Hassan <54103753+Moha143@users.noreply.github.com>
2023-10-23 12:25:39 +0300

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PROJECT 3 MAIL CS50
+# MAIL
 Design a front-end for an email client that makes API calls to send and receive emails.
 
 
--- mail/admin.py
+++ mail/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from mail.models import Email
+from mail.models import Email,User
 # Register your models here.
 
 admin.site.register(Email)
+admin.site.register(User)
